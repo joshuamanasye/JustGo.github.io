58296434ed1b0d9424f450dcce5407511e0e8d5e
Joshua <103163689+joshuamanasye@users.noreply.github.com>
2023-06-11 15:17:14 +0700

    carousel done + ganti gmbr slide 5

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,45 @@
 var current = 0;
+var imageSliderContainer = $('#image-slider-container');
 var slides = $('.slider-content');
 var carousel = $('#carousel');
+var content = null;
+
+// let content = [
+//     {
+//         name: 'Great Ocean Road',
+//         location: 'Australia',
+//         desc: "A mesmerizing coastal drive unveiling stunning cliffs, golden beaches, and iconic landmarks along Australia's southeastern coast.",
+//         imgUrl: '../assets/slider/1.jpg'
+//     },
+
+//     {
+//         name: 'Machu Picchu',
+//         location: 'Peru',
+//         desc: "A mystical Incan citadel in the Andes, boasting awe-inspiring ruins and breathtaking mountain views.",
+//         imgUrl: '../assets/slider/2.jpg'
+//     },
+
+//     {
+//         name: 'Gros Piton',
+//         location: 'St. Lucia',
+//         desc: "A majestic mountain peak in Saint Lucia, rising dramatically from the Caribbean Sea, showcasing stunning natural beauty and providing a challenging yet rewarding hiking experience.",
+//         imgUrl: '../assets/slider/3.jpg'
+//     },
+
+//     {
+//         name: 'Bora Bora',
+//         location: 'French Polynesia',
+//         desc: "A tropical paradise known for its pristine turquoise waters, white sandy beaches, and overwater bungalows, offering a dream-like setting for relaxation and indulgence.",
+//         imgUrl: '../assets/slider/4.jpg'
+//     },
+
+//     {
+//         name: 'Oia',
+//         location: 'Greece',
+//         desc: "Santorini's enchanting cliffside village, famous for its captivating sunsets, iconic blue-domed churches, and whitewashed architecture, creating a mesmerizing scene overlooking the azure Aegean Sea.",
+//         imgUrl: '../assets/slider/5.jpg'
+//     }
+// ];
 
 // console.log(carousel);
 // console.log(slides);
@@ -16,11 +55,27 @@ function scrollCarousel() {
     // carousel.animate({left: parseInt(parseInt($('.slider-content').width() + parseInt($('.slider-content').css('marginLeft')) * 2) * current * -1 + 15) + 'px'}, 400, 'swing');
 }
 
+function changeCarouselDetail() {
+    imageSliderContainer.css('background-position-x', 25 * current + '%');
+
+    setTimeout(function() {
+        imageSliderContainer.css('background-image', 'url(' + content[current].imgUrl + ')');
+    
+        $('#slider-text > h2').html(content[current].name);
+        $('#slider-text > p').html(content[current].desc);
+    }, 100);
+
+    // sliderText.eq(current).children[0].innerHTML = content[current].title;
+    // sliderText.eq(current).children[1].innerHTML = content[current].desc;
+}
+
 function changeActive(target) {
     slides[current].classList.remove('active-slider');
     current = target;
     scrollCarousel();
     slides[current].classList.add('active-slider');
+
+    changeCarouselDetail();
 }
 
 // sekedar curhatan: knp transition sama jquery animation tuh nabrak
@@ -84,7 +139,7 @@ function initSlides() {
     }
 }
 
-initSlides();
+// initSlides();
 
 // carousel control
 $('#arrow-left').click(scrollLeft);
@@ -98,4 +153,16 @@ $('html').keyup(function (e) {
 // benerin abis resize window
 $(window).on('resize', function() {
     setTimeout(scrollCarousel, 400); // setTimeout biarin animasinya yg lain kelar dulu
+});
+
+function setListener() {
+
+}
+
+fetch("./js/slider-content.json").then((res) => {
+    return res.json();
+}).then((data) => {
+    content = data;
+    initSlides();
+    setListener();
 });
